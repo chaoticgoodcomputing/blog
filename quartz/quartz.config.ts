581fae24be85f279cccf3337343ff93542d11a83
Spencer Elkington <spelkington@gmail.com>
2025-04-28 21:49:47 -0600

    feat: quartz core restored, vault ported

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.chaoticgood.computer",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
